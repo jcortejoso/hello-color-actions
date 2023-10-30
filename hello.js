53abaca27c12754e85caaaca43a002f21dae7b19
Javier Cortejoso <javier.cortejoso@gmail.com>
2023-10-30 17:38:45 +0100

    Update webpage

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from the Office Hours in the terminal at October 30th!";
 console.log(hello());
 exports.hello = hello;
